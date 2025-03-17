1946e40f623189ed284e56964842df43071c6aa4
Joseph Fonseka <hevayo@gmail.com>
2025-03-17 22:59:55 +0530

    Add component yaml

--- bffapi/index.js
+++ bffapi/index.js
@@ -8,7 +8,7 @@ const { parserRouter } = require('./src/routes/billParser');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 9090;
 
 // Middleware
 app.use(cors());
